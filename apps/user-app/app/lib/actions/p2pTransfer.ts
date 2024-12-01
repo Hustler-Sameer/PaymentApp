3e277dd264e7140488936974201b58609f362bfa
Hustler-Sameer <sameerkurkure10@gmail.com>
2024-12-01 23:32:36 +0530

    v2

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -5,6 +5,11 @@ import prisma from "@repo/db/client";
 
 export async function p2pTransfer(to: string, amount: number) {
     // this code has a bug , if we hit 2 request simultaneously then there is a chance that balance of one user may go negative
+
+    //now adding locking mechanism to allow only 1 request to go out
+
+
+
     const session = await getServerSession(authOptions);
     const from = session?.user?.id;
     if (!from) {
@@ -24,6 +29,8 @@ export async function p2pTransfer(to: string, amount: number) {
         }
     }
     await prisma.$transaction(async (tx) => {
+        //this will lock the row to avoid simultaneously 
+        await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
         const fromBalance = await tx.balance.findUnique({
             where: { userId: Number(from) },
           });
@@ -40,5 +47,16 @@ export async function p2pTransfer(to: string, amount: number) {
             where: { userId: toUser.id },
             data: { amount: { increment: amount } },
           });
+          //tracking transaction of peer to peer
+          await tx.p2pTransfer.create({
+            data:{
+                fromUserId:Number(from),
+                toUserId:toUser.id,
+                amount,
+                timestamp:new Date()
+            }
+          })
+          
+          
     });
 }
